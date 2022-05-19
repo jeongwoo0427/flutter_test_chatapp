dd26a1bc41902c28877ce2b044484c842afc8772
BamBoo <64342840+jwk9022648@users.noreply.github.com>
2022-05-19 10:37:51 +0900

    메시지 입력 기능 구현

--- lib/screen/message_list_screen.dart
+++ lib/screen/message_list_screen.dart
@@ -12,10 +12,12 @@ class MessageListScreen extends StatefulWidget {
 }
 
 class _MessageListScreenState extends State<MessageListScreen> {
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('메세지 목록')),
+      appBar: AppBar(title: const Text('메세지 목록')),
       body: StreamBuilder<List<MessageModel>>(
         stream: streamMessages(), //중계하고 싶은 Stream을 넣는다.
         builder: (context, asyncSnapshot) {
@@ -28,17 +30,92 @@ class _MessageListScreenState extends State<MessageListScreen> {
             );
           } else {
             List<MessageModel> messages = asyncSnapshot.data!; //비동기 데이터가 존재할 경우 리스트뷰 표시
-            return ListView.builder(
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(messages[index].content),
-                  );
-                });
+            return Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                    child: ListView.builder(
+                        // 상위 위젯의 크기를 기준으로 잡는게 아닌 자식위젯의 크기를 기준으로 잡음
+                        itemCount: messages.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(messages[index].content),
+                          );
+                        })),
+                getInputWidget()
+              ],
+            );
           }
         },
       ),
     );
+  }
+
+  Widget getInputWidget() {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(boxShadow: const [
+        BoxShadow(color: Colors.black12, offset: Offset(0, -2), blurRadius: 3)
+      ], color: Theme.of(context).bottomAppBarColor),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15,vertical: 8),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(fontSize: 15),
+                  labelText: "내용을 입력하세요..",
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    borderSide: BorderSide(
+                      color: Colors.black26,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 10,),
+            RawMaterialButton(
+              onPressed: _onPressedSendButton,
+              constraints: BoxConstraints(
+                minWidth: 0,
+                minHeight: 0
+              ),
+              elevation: 2,
+              fillColor: Theme.of(context).colorScheme.primary,
+              shape: CircleBorder(),
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Icon(Icons.send),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onPressedSendButton(){
+    try{
+      MessageModel messageModel = MessageModel(content: controller.text);
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      firestore.collection('chatrooms/YLCoRBj59XRsDdav2YV1/messages').add(messageModel.toMap());
+
+    }catch(ex){
+      log('error)',error: ex.toString(),stackTrace: StackTrace.current);
+    }
   }
 
   Stream<List<MessageModel>> streamMessages(){
@@ -52,7 +129,10 @@ class _MessageListScreenState extends State<MessageListScreen> {
         List<MessageModel> messages = [];//querySnapshot을 message로 옮기기 위해 List<MessageModel> 선언
         querySnapshot.docs.forEach((element) { //해당 컬렉션에 존재하는 모든 docs를 순회하며 messages 에 데이터를 추가한다.
           messages.add(
-              MessageModel.fromMap(element.data() as Map<String, dynamic>)
+              MessageModel.fromMap(
+                  id:element.id,
+                  map:element.data() as Map<String, dynamic>
+              )
           );
         });
         return messages; //QuerySnapshot에서 List<MessageModel> 로 변경이 됐으니 반환
